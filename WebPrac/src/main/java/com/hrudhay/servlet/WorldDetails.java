9b1661df391080f7eae59465761964de77daef30
hrudh <hrudh@Hrudhay>
2020-12-08 18:42:17 +0530

    added creating file with radio button option

--- WebPrac/src/main/java/com/hrudhay/servlet/WorldDetails.java
+++ WebPrac/src/main/java/com/hrudhay/servlet/WorldDetails.java
@@ -1,6 +1,9 @@
 package com.hrudhay.servlet;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
@@ -19,9 +22,13 @@ public class WorldDetails extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id = request.getParameter("ID");
+		String file = request.getParameter("File");
 		WorldDao wod = new WorldDao();
 		try {
 			World wo = wod.getWorld(id);
+			if(file.contains("yes")) {
+				CreateFile(wo);
+			}
 			request.setAttribute("idCountry", wo);
 			RequestDispatcher rd = request.getRequestDispatcher("showWorldDetails.jsp");
 			rd.forward(request, response);
@@ -29,6 +36,14 @@ public class WorldDetails extends HttpServlet {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	void CreateFile(World wo) throws IOException {
+		File file = new File("D:\\EE WorkSpacee\\test write files\\World.txt");
+		FileWriter fw = new FileWriter(file);
+		PrintWriter pw = new PrintWriter(fw);
+		pw.write(wo.getID()+" "+wo.getName()+" "+wo.getCountryCode()+" "+wo.getDistrict()+" "+wo.getPopulation()+"\n");
+		pw.close();
 	}
 	
 }
--- WebPrac/src/main/webapp/index.jsp
+++ WebPrac/src/main/webapp/index.jsp
@@ -1,8 +1,10 @@
 <html>
 <body>
-<h2>Hello India!</h2>
+	<h2>Hello India!</h2>
 	<form action="getWorld">
-		<input type="text" name="ID">
+		<input type="text" name="ID"><br> 
+		<input type="radio" name="File" value="yes" />Enter in File
+		<input type="radio" name="File" value="no" />Don't Enter in File<br>
 		<input type="submit">
 	</form>
 </body>
